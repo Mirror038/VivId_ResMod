900f6bfbbf88a8e92b43ef007b6a2146f7d2a635
F89465586 <2914938850@qq.com>
2023-08-07 16:53:30 +0800

    Initial commit

---
+++ src/main/java/name/vivid_resmod/VivId_ResModClient.java
@@ -0,0 +1,10 @@
+package name.vivid_resmod;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class VivId_ResModClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
---
+++ src/main/java/name/vivid_resmod/VivId_ResModDataGenerator.java
@@ -0,0 +1,11 @@
+package name.vivid_resmod;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class VivId_ResModDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
